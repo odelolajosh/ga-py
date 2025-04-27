fe6e4e0f009098bfcf55ef6630416ef95ee11ff4
Joshua Odelola <oladapoodelola@gmail.com>
2025-04-27 17:23:07 +0100

    chore: run just the lab exercise

--- app.py
+++ app.py
@@ -28,28 +28,6 @@ class PlotFitnessCallback(Callback):
         plt.show()
 
 
-# print(f"Solving a basic f(x1, x2) = x1^2 x2 + 2x1 - x2")
-# ga = GeneticAlgorithm(
-#     population_size=100,
-#     objective_function=lambda x: (x[0]**2)*x[1] + 2*x[0] - x[1],
-#     chromosome_decoder=BinaryChromosomeDecoder(
-#         number_of_bytes=6,
-#         number_of_decision_variables=2,
-#         lower_bounds=[2, -1],
-#         upper_bounds=[6, 4]
-#     ),
-#     termination=OrTermination(
-#         NumberOfGeneration(20),
-#         ThresholdDifference(0.05)
-#     ),
-#     optimization=Maximization(),
-#     selection=RouletteSelection(),
-#     crossover=SinglePointCrossover(0.85),
-#     mutation=BitFlipMutation(0.2),
-# )
-# ga.run()
-# print(ga.result)
-
 print(f"Laboratory Exercise 1")
 def f(x):
     return 2*x[0]*x[1]*x[2] - 4*x[0]*x[2] - 2*x[1]*x[2] + x[0]**2 + x[1]**2 + x[2]**2 - 2*x[0] - 4*x[1] + 4*x[2]
